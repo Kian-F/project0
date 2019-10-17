1b127168340cca1a7a9949703b92a46586a58f9d
Kian <kian.info78@gmail.com>
2019-10-17 17:56:31 +1100

    ver01

--- js/logic.js
+++ js/logic.js
@@ -1,19 +1,26 @@
 var markers = ["X", "O"];
-var turn = 0;
-var player = $('#nameTag').text();
-var status = "active";
-var score = 0;
+let turn = 0;
+let player = $('#nameTag').text();
+let status = "active";
+let scoreX = 0;
+let scoreO = 0;
+let counter = 0;
 
-// function playGame(culId) {
 $('.column').on('click', function() {
   if ($(this).text().length > 0) return;
   if (status === "stop") return;
   $(this).text(markers[turn]);
   if (turn == 1) turn = 0;
   else turn = 1;
+  counter++;
+  if (counter === 9) {
+    console.log("Draw");
+  }
   win();
 
 })
+
+
 
 function winerColor(c0, c1, c2) {
   c0.addClass("win");
@@ -31,50 +38,54 @@ function win() {
   const col6 = $("#col6");
   const col7 = $("#col7");
   const col8 = $("#col8");
+  let winer = "";
 
   if (col0.text() != '' && col0.text() === col1.text() && col1.text() === col2.text()) {
     winerColor(col0, col1, col2);
-    status = "stop";
+    winer = col0.text();
   } else if (col3.text() != '' && col3.text() === col4.text() && col4.text() === col5.text()) {
     winerColor(col3, col4, col5);
-    status = "stop";
+
   } else if (col6.text() != '' && col6.text() === col7.text() && col7.text() === col8.text()) {
     winerColor(col6, col7, col8);
-    status = "stop";
+
   } else if (col0.text() != '' && col0.text() === col3.text() && col3.text() === col6.text()) {
     winerColor(col0, col3, col6);
-    status = "stop";
+
   } else if (col1.text() != '' && col1.text() === col4.text() && col4.text() === col7.text()) {
     winerColor(col1, col4, col7);
-    status = "stop";
+
   } else if (col2.text() != '' && col2.text() === col5.text() && col5.text() === col8.text()) {
     winerColor(col2, col5, col8);
-    status = "stop";
+
   } else if (col0.text() != '' && col0.text() === col4.text() && col4.text() === col8.text()) {
     winerColor(col0, col4, col8);
-    status = "stop";
+
   } else if (col2.text() != '' && col2.text() === col4.text() && col4.text() === col6.text()) {
     winerColor(col2, col4, col6);
+
+  }
+  if (winer === 'X') {
+    scoreX++;
     status = "stop";
+
+  } else if (winer === 'O') {
+    scoreO++;
+    status = "stop";
+
   }
 }
 
-// $(document).ready(function() {
-//   const grid = [
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8],
-//     [0, 3, 6],
-//     [1, 4, 7],
-//     [2, 5, 8],
-//     [0, 4, 8],
-//     [2, 4, 6]
-//   ];
-//   const columns = document.querySelectorAll('.column');
-//   //  startGame();
-//   board =
-//
-//     $('.column').click(function() {
-//       $(this).html(playerChoice);
-//     });
-// });
+function restart() {
+  status = "active";
+  turn = 0;
+  counter = 0;
+  $('.column').removeClass('win').empty()
+
+}
+
+
+
+$('#restart').on('click', function() {
+  restart();
+});
